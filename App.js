81b42b8d527025ad2df74da6429d59adf9d2279a
Paul Walkington <paulwalkington@nimbleapproach.com>
2023-03-10 11:54:17 +0000

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View, Text } from "react-native";
+import { Component } from "react";
+
+import {
+  Button,
+  ButtonWithCount,
+  ButtonWithHookCount,
+} from "./components/Button";
+import ImageViewer from "./components/ImageViewer";
+import { useState } from "react";
+
+const PlaceholderImage = require("./assets/images/background-image.png");
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.imageContainer}>
+        <ImageViewer placeholderImageSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label="Choose a photo" />
+        {/* <Button label="Use this photo" /> */}
+        {/* <TextInANest></TextInANest> */}
+        <ButtonWithCount />
+        <ButtonWithHookCount />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +33,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#25292e",
+    alignItems: "center",
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: "center",
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  image: {
+    width: 320,
+    height: 200,
+    borderRadius: 18,
   },
 });
